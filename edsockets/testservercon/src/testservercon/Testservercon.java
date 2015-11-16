ff8fc236feabe0dbb8d2487532ce57291bf9a991
ed <changethefrequency@hotmail.com>
2015-11-16 13:55:18 +0100

    Java socket listener
    Copy paste van een simpele Java Listener (server) die lijkt te werken.

---
+++ edsockets/testservercon/src/testservercon/Testservercon.java
@@ -0,0 +1,53 @@
+/*
+ * Java socketluisteraartest.
+ * verwacht input in vorm van ints tussen 0 en 5, stopt bij 4 en "exit"
+ */
+package testservercon;
+ 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.ClassNotFoundException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author 54
+ */
+public class Testservercon {
+
+    //static ServerSocket variable
+    private static ServerSocket server;
+    //socket server port on which it will listen
+    private static int port = 9876;
+     
+    public static void main(String args[]) throws IOException, ClassNotFoundException{
+        //create the socket server object
+        server = new ServerSocket(port);
+        //keep listens indefinitely until receives 'exit' call or program terminates
+        while(true){
+            System.out.println("Waiting for client request");
+            //creating socket and waiting for client connection
+            Socket socket = server.accept();
+            //read from socket to ObjectInputStream object
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            //convert ObjectInputStream object to String
+            String message = (String) ois.readObject();
+            System.out.println("Message Received: " + message);
+            //create ObjectOutputStream object
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+            //write object to Socket
+            oos.writeObject("Hi Client "+message);
+            //close resources
+            ois.close();
+            oos.close();
+            socket.close();
+            //terminate the server if client sends exit request
+            if(message.equalsIgnoreCase("exit")) break;
+        }
+        System.out.println("Shutting down Socket server!!");
+        //close the ServerSocket object
+        server.close();
+    }  
+}
