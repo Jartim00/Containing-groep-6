bd046e6d1d68279cbcfeefea998442f45b534879
robinpranger <pran1301@nhl.nl>
2015-11-17 10:24:38 +0100

    zo connect je met de database

---
+++ connect.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+	function Connection(){
+		$server='raspberry ip:3306';
+		$user='jaron';
+		$pass='containing';
+		$db='naam van de database';
+
+		$connection = mysql_connect($server, $user, $pass);
+
+		if (!$connection) {
+	   	die('TEST ' . mysql_error());
+		}
+
+		mysql_select_db($db) or die( 'MySQL ERROR: '. mysql_error() );
+		return $connection;
+	}
+?>
